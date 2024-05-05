9b7cf1bce6983efaf20c31ee536d119229ddb62b
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-06 01:07:54 +0530

    Guaid started

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
 
 const Guide = () => {
   return (
-    <div>Guide</div>
-  )
-}
+    <section className="flexCenter flex-col">
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+        <Image 
+        src='/camp.svg'
+        alt="camp"
+        width={50}
+        height={50}
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Guide
+export default Guide;
