d7ff287b248fc723a7c0bbbbe6f281c1c753ab50
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-06 11:42:04 +0530

    vercel setting created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,9 @@
 
 const Features = () => {
   return (
-    <div>Features</div>
+   <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-no-repeat py-24">
+
+   </section>
   )
 }
 
