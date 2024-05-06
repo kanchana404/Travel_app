8be18160a49168a3771d355669e541413279b016
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-06 12:28:56 +0530

    again vercel settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
